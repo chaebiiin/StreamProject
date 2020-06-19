197fdbf3139e9e472faf9afc925058d0758c2890
cheabiiin <ravis_toy@naver.com>
2020-06-19 21:33:21 +0900

    20-06-19 스트림 확인문제5

--- StreamProject/src/treminal/StreamExample.java
+++ StreamProject/src/treminal/StreamExample.java
@@ -2,31 +2,42 @@ package treminal;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 public class StreamExample {
 	public static void main(String[] args) {
-		List<String> list = Arrays.asList("Lamda Expressions", 
-										"Java8 supports lamda expressions",
-										"This is a java book");
+		List<String> list = Arrays.asList("Lamda Expressions", "Java8 supports lamda expressions",
+				"This is a java book");
 
 		list.stream().flatMap(new Function<String, Stream<String>>() {
 			@Override
 			public Stream<String> apply(String t) {
-//				Stream<String> listAry = t.toUpperCase();
-				return null;
+				return Stream.of(t.toUpperCase());
 			}
-		});
-
-		list.stream().filter(new Predicate<String>() {
+		}).filter(new Predicate<String>() {
 
 			@Override
 			public boolean test(String t) {
-//				System.out.println(t);
-				return t.indexOf("ava") > 0;
+				return t.indexOf("JAVA") >= 0;
 			}
-		}).forEach(s -> System.out.println(s));
+		}).forEach(new Consumer<String>() {
+
+			@Override
+			public void accept(String t) {
+				System.out.println(t);
+			}
+		});
+
+//		list.stream().filter(new Predicate<String>() {
+//
+//			@Override
+//			public boolean test(String t) {
+////				System.out.println(t);
+//				return t.indexOf("ava") > 0;
+//			}
+//		}).forEach(s -> System.out.println(s));
 	}
 }
